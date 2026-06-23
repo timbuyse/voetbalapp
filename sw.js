f460a18a68c903e3509e0d35a6b3fa370aef73e3
timbuyse <buysesorgeloos@gmail.com>
2026-06-23 14:50:49 +0200

    Tornooien: tornooi aanmaken met ploeg/selectie, wedstrijden per tornooi, tornooi-tile op home

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'voetbal-v21';
+const CACHE = 'voetbal-v22';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './logo-default.svg'];
 
 self.addEventListener('install', e => {
